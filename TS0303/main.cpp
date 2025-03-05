d923b42ebe0c6fccad06649ca2e1b4e3edac8990
xinshoutw <admin@xinshou.tw>
2025-03-05 16:15:28 +0800

    feat: add week3 lab-class quiz

--- TS0303/main.cpp
+++ TS0303/main.cpp
@@ -1,17 +1,52 @@
+#include <complex>
 #include <iostream>
 #include <cstdint>
 #include <iomanip>
+#include <cstring>
 
 int main() {
-    double input_double; // X
-    int32_t deciLimit; // N
+    std::string input; // X
+    int32_t decimal_length_limit; // N
     int32_t full_length_limit; // M
-    char padding; // C
 
-    while (std::cin >> input_double >> full_length_limit >> deciLimit >> padding) {
-        std::cout.precision(deciLimit);
-        std::cout <<
-            std::setw(full_length_limit) << std::setfill(padding) << std::right << std::fixed << input_double << '\n';
+    while (std::cin >> input >> full_length_limit >> decimal_length_limit) {
+        getchar();
+        const uint8_t padding = getchar(); // C
+        const size_t find_index = input.find('.');
+        const bool have_decimal_point = find_index != std::string::npos;
+        const auto whole_number_length = static_cast<int32_t>(have_decimal_point ? find_index : input.length());
+        const int32_t decimal_length = decimal_length_limit + (decimal_length_limit != 0); // include decimal point
+        const int32_t padding_length = full_length_limit - whole_number_length - decimal_length;
+
+        // print padding
+        for (int i = 0; i < padding_length; ++i) {
+            putchar(padding);
+        }
+
+        // print whole-number
+        if (have_decimal_point) {
+            std::cout << input.substr(0, find_index);
+        } else {
+            std::cout << input;
+        }
+
+        // print decimal part (include decimal point)
+        int32_t ctn = 0;
+        if (decimal_length_limit != 0 && !have_decimal_point) {
+            ctn++;
+            std::cout << '.';
+        } else {
+            for (int output_i = static_cast<int32_t>(find_index);
+                 ctn < decimal_length && input[output_i] != '\0';
+                 ++ctn, ++output_i) {
+                std::cout << input[output_i];
+            }
+        }
+
+        for (; ctn < decimal_length; ++ctn) {
+            putchar('0');
+        }
+        std::cout << '\n';
     }
 
     return 0;
---
+++ TS0305/main.cpp
@@ -0,0 +1,157 @@
+#include <cassert>
+#include <iostream>
+#include <cstdint>
+#include <iomanip>
+
+uint64_t kBorderHeight;
+uint64_t kBorderWidth;
+bool* kCanvas;
+
+void DrawCanvas() {
+    for (int y = 0; y < kBorderHeight; ++y) {
+        for (int x = 0; x < kBorderWidth; ++x) {
+            if (kCanvas[y * kBorderWidth + x]) {
+                std::cout << "X";
+            } else {
+                std::cout << "*";
+            }
+        }
+        std::cout << '\n';
+    }
+    std::cout << '\n';
+}
+
+void ProcessDrawSquare() {
+    int64_t length;
+    int64_t x1, y1;
+    std::cin >> length >> x1 >> y1;
+
+    // border check
+    if ((x1 < 0) ||
+        (y1 < 0) ||
+        (x1 + length > kBorderWidth) ||
+        (y1 + length > kBorderHeight)
+    ) {
+        std::cout << "Out of range.\n\n";
+        return;
+    }
+
+    for (int y = 0; y < length; ++y) {
+        std::fill_n(kCanvas + (x1 + (y1 + y) * kBorderWidth), length, true);
+    }
+
+    DrawCanvas();
+}
+
+void ProcessDrawLine() {
+    int64_t x1, y1;
+    int64_t x2, y2;
+    std::cin >> x1 >> y1 >> x2 >> y2;
+
+
+    // border check
+    if ((x1 < 0) || (x1 >= kBorderWidth) ||
+        (y1 < 0) || (y1 >= kBorderHeight) ||
+        (x2 < 0) || (x2 >= kBorderWidth) ||
+        (y2 < 0) || (y2 >= kBorderHeight)
+    ) {
+        std::cout << "Out of range.\n\n";
+        return;
+    }
+
+    if (x1 == x2) { // vertical
+        const int64_t up_y = std::min(y1, y2);
+        const int64_t down_y = std::max(y1, y2);
+
+        for (int64_t y = up_y; y <= down_y; ++y) {
+            kCanvas[x1 + y * kBorderWidth] = true;
+        }
+    } else if (y1 == y2) { // horizon
+        const int64_t x_left = std::min(x1, x2);
+        const int64_t x_right = std::max(x1, x2);
+        std::fill_n(kCanvas + (y1 * kBorderWidth + x_left), x_right - x_left + 1, true);
+    } else {
+        // use vector to make (x1, y1) approach to (x2, y2)
+        const int64_t distance = x2 - x1 + 1;
+        const int64_t x_offset = (x2 - x1) > 0 ? 1 : -1; // point1 -> point2
+        const int64_t y_offset = (y2 - y1) > 0 ? 1 : -1; // point1 -> point2
+        int64_t new_x1 = x1;
+        int64_t new_y1 = y1;
+
+        for (int i = 0; i < distance; ++i) {
+            kCanvas[new_x1 + new_y1 * kBorderWidth] = true;
+            new_x1 += x_offset;
+            new_y1 += y_offset;
+        }
+    }
+
+    DrawCanvas();
+}
+
+void ProcessDrawTriangle() {
+    int64_t length;
+    int64_t x1, y1;
+    char facing[3]; // include '\0'
+    std::cin >> length >> x1 >> y1 >> facing;
+
+    // border check
+    if ((x1 < 0) || (y1 < 0)) {
+        std::cout << "Out of range.\n\n";
+        return;
+    }
+
+    if (facing[0] == 'R') {
+        if (x1 + length > kBorderWidth) {
+            std::cout << "Out of range.\n\n";
+            return;
+        }
+    } else {
+        if ((x1 - length) < -1) {
+            std::cout << "Out of range.\n\n";
+            return;
+        }
+    }
+
+    if (facing[1] == 'D') {
+        if (y1 + length > kBorderHeight) {
+            std::cout << "Out of range.\n\n";
+            return;
+        }
+    } else {
+        if ((y1 - length) < -1) {
+            std::cout << "Out of range.\n\n";
+            return;
+        }
+    }
+
+    for (int y = 0; y < length; ++y) {
+        for (int x = 0; x < length - y; ++x) {
+            kCanvas[
+                (y1 + ((facing[1] == 'D') ? y : -y)) * kBorderWidth +
+                (x1 + ((facing[0] == 'R') ? x : -x))
+            ] = true;
+        }
+    }
+
+    DrawCanvas();
+}
+
+int main() {
+    std::cin >> kBorderWidth >> kBorderHeight;
+    kCanvas = new bool[kBorderHeight * kBorderWidth]{false};
+
+    std::string input_draw_type;
+    while (std::cin >> input_draw_type) {
+        if (input_draw_type == "S") {
+            ProcessDrawSquare();
+        } else if (input_draw_type == "L") {
+            ProcessDrawLine();
+        } else if (input_draw_type == "T") {
+            ProcessDrawTriangle();
+        } else if (input_draw_type == "EXIT") {
+            break;
+        }
+    }
+
+    return 0;
+}
